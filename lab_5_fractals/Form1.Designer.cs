0fd81d133f719bcbf77c53255746a9b3bb1fc0a8
Ксения Голубева <golubeva3.ka@edu.spbstu.ru>
2021-04-03 01:57:05 +0300

    Add project files.

---
+++ lab_5_fractals/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace lab_5_fractals
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(36, 95);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(525, 487);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(190, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Исходное изображение:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(623, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(159, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Сегментированное:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(627, 95);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(525, 487);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1202, 782);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ lab_5_fractals/Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using System.Windows.Forms;
+
+
+namespace lab_5_fractals
+{
+    public partial class Form1 : Form
+    {
+
+        const int cellSize = 10;
+        int celllInLine, cellInCol;
+        int cellCount;
+        float[,,] u0, u, b;
+        int w, h;
+        const int delta = 4;
+        //double[] xi = new double[delta];
+        //double[] yi = new double[delta];
+        float[,] vol;
+        float[] area;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string path = "1.jpg";
+            Bitmap img = new Bitmap(path);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.Image = img;
+            h = img.Height;
+            w = img.Width;
+            celllInLine = w / cellSize;
+            cellInCol = h / cellSize;
+            cellCount = celllInLine * cellInCol;
+            u0 = new float[cellCount, cellSize, cellSize];
+            u = new float[cellCount, cellSize, cellSize];
+            b = new float[cellCount, cellSize, cellSize];
+
+            vol = new float[cellCount, delta];
+            area = new float[cellCount];
+            setU0(img);
+            float s;
+            int d;
+            //calc u and b 2 times
+            for (d = 1; d <= delta; d++)
+            {
+                calcU(d);
+                calcB(d);
+                for (int k = 0; k < cellCount; k++)
+                {
+                    s = 0;
+                    for (int i = cellSize - 1; i >= 0; i--)
+                    {
+                        for (int j = cellSize - 1; j >= 0; j--)
+                        {
+                            s += u[k, i, j] - b[k, i, j];
+                        }
+                    }
+                    vol[k, d - 1] = s;
+                }
+
+            }
+            d = delta - 1;
+            s = 0f;
+            for (int k = 0; k < cellCount; k++)
+            {
+                area[k] = (vol[k, d] - vol[k, d - 1]) / 2;
+                s += area[k];
+            }
+            s /= cellCount;
+            for (int k = 0; k < cellCount; k++)
+            {
+                try { 
+                    if (area[k] < s)
+                    {
+                        for (int i = cellSize - 1; i >= 0; i--)
+                        {
+                            for (int j = cellSize - 1; j >= 0; j--)
+                            {
+                                img.SetPixel(i + (cellSize * (k % celllInLine)), j + (cellSize * (k / celllInLine)), Color.White);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        for (int i = cellSize - 1; i >= 0; i--)
+                        {
+                            for (int j = cellSize - 1; j >= 0; j--)
+                            {
+                                img.SetPixel(i + (cellSize * (k % celllInLine)), j + (cellSize * (k / celllInLine)), Color.Black);
+                            }
+                        }
+                    }
+                } catch (Exception exception) {
+
+                }
+            }
+            img.Save("result.png");
+            string path2 = "result.png";
+            Bitmap img2 = new Bitmap(path2);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.Image = img2;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void setU0(Bitmap img)
+        {
+            for (int k = 0; k < cellCount; k++)
+            {
+                for (int i = cellSize - 1; i >= 0; i--)
+                {
+                    for (int j = cellSize - 1; j >= 0; j--)
+                    {
+                        u0[k, i, j] = getIntense(img.GetPixel(i + (cellSize * (k % celllInLine)), j + (cellSize * (k / celllInLine))));
+                    }
+                }
+            }
+
+            return;
+        }
+        //верхняя поверхность uδ(i,j)
+        private void calcU(int delta)
+        {
+            if (delta > 1)
+            {
+                for (int k = 0; k < cellCount; k++)
+                {
+                    for (int i = cellSize - 1; i >= 0; i--)
+                    {
+                        for (int j = cellSize - 1; j >= 0; j--)
+                        {
+                            u0[k, i, j] = u[k, i, j];
+                        }
+                    }
+                }
+            }
+            for (int k = 0; k < cellCount; k++)
+            {
+                for (int i = cellSize - 1; i >= 0; i--)
+                {
+                    for (int j = cellSize - 1; j >= 0; j--)
+                    {
+                        u[k, i, j] = Math.Max(
+                            u0[k, i, j] + 1,
+                            Math.Max(
+                                Math.Max(
+                                    i > 0 ? u0[k, i - 1, j] : 0,
+                                    j > 0 ? u0[k, i, j - 1] : 0
+                                    ),
+                                Math.Max(
+                                    i < cellSize - 1 ? u0[k, i + 1, j] : 0,
+                                    j < cellSize - 1 ? u0[k, i, j + 1] : 0
+                                    )
+                                )
+                            );
+                    }
+                }
+            }
+            return;
+        }
+
+        //нижняя поверхность uδ(i,j)
+        private void calcB(int delta)
+        {
+            if (delta > 1)
+            {
+                for (int k = 0; k < cellCount; k++)
+                {
+                    for (int i = cellSize - 1; i >= 0; i--)
+                    {
+                        for (int j = cellSize - 1; j >= 0; j--)
+                        {
+                            u0[k, i, j] = b[k, i, j];
+                        }
+                    }
+                }
+            }
+            for (int k = 0; k < cellCount; k++)
+            {
+                for (int i = cellSize - 1; i >= 0; i--)
+                {
+                    for (int j = cellSize - 1; j >= 0; j--)
+                    {
+                        b[k, i, j] = Math.Min(
+                            u0[k, i, j] - 1,
+                            Math.Min(
+                                Math.Min(
+                                    i > 0 ? u0[k, i - 1, j] : 256,
+                                    j > 0 ? u0[k, i, j - 1] : 256
+                                    ),
+                                Math.Min(
+                                    i < cellSize - 1 ? u0[k, i + 1, j] : 256,
+                                    j < cellSize - 1 ? u0[k, i, j + 1] : 256
+                                    )
+                                )
+                            );
+                    }
+                }
+            }
+            return;
+        }
+
+        private float getIntense(Color color)
+        {
+            return (color.R + color.G + color.B) / 3;
+        }
+    }
+}
